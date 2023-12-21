01cb2a31800acd4b65b6d15f939a571b0e25904a
AnujaDivekar27 <divekar.anuja27@gmail.com>
2023-12-22 00:56:52 +0530

    SpringProject1 added

---
+++ SprinB/SpringBootJConfig/src/main/java/com/example/demo/SpringBootJConfigApplication.java
@@ -0,0 +1,17 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJConfigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootJConfigApplication.class, args);
+	
+	System.out.println("==========================================");
+	System.out.println("Configuration using application.resource ");
+	
+	}
+
+}
---
+++ SprinB/SpringBootJConfig/src/main/java/com/example/demo/entity/Employee.java
@@ -0,0 +1,58 @@
+package com.example.demo.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Employee {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+
+	private String name;
+
+	private String address;
+
+	private String company;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getCompany() {
+		return company;
+	}
+
+	public void setCompany(String company) {
+		this.company = company;
+	}
+
+	@Override
+	public String toString() {
+		return "Employee [id=" + id + ", name=" + name + ", address=" + address + ", company=" + company + "]";
+	}
+
+}
